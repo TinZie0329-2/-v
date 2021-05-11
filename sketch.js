7cadc817046b43c2eca71cd49d7858d3265c72fc
TinZie0329-2 <83267439+TinZie0329-2@users.noreply.github.com>
2021-05-11 15:51:24 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://tinzie0329-2.github.io/-v/model.json';
 
 
 function preload() {
